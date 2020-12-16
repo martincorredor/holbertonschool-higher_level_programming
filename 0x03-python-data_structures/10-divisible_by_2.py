d7c1e794f8b635d9e19b43efdb638cecced11f83
martincorredor <dev.martincorredor@gmail.com>
2020-12-15 20:36:38 -0500

    Finds multiples by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list.copy()
-    for i in new_list:
-        if (i%2) == 0:
-            new_list[i] = True
+    cp = my_list.copy()
+    for run in range(len(cp)):
+        if cp[run] % 2 == 0:
+            cp[run] = True
         else:
-            new_list[i] = False
-    return new_list
+            cp[run] = False
+    return cp
