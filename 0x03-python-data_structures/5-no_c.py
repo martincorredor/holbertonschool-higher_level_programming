76b6a901c55efefe4256cc14f08a4424e7b53dde
martincorredor <dev.martincorredor@gmail.com>
2020-12-15 19:57:53 -0500

    Remove character of a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    aux = list(my_string)
-    for i in aux:
-        if i == 'c' or i == 'C':
-            aux.remove(i)
-    new_str = "".join(aux)
-    return new_str
+    return (my_string.translate({ord('c'): None, ord('C'): None}))
