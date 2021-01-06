88b7f97f56cb15eaa67a146ec48bf1383f95c869
martincorredor <dev.martincorredor@gmail.com>
2021-01-05 19:47:18 -0500

    Verifies the size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,13 +6,8 @@ Define class Square
 
 
 class Square:
-    """
-    Define a square
-    Args:
-        size : Side of a square
-    """
+    """Class to defines a square"""
     def __init__(self, size = 0):
-        """Class to defines a square"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
