1dd70536102e51a5d99cdeea58b21af8145ce7ce
martincorredor <dev.martincorredor@gmail.com>
2021-01-05 19:05:16 -0500

    Initializes a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,12 +7,14 @@ This module contain the Square class
 class Square:
     """
     Defines a square
+    Args:
+        size: Side of the square
     """
     def __init__(self, size):
         """
         Initializes the square
-        Args:
-            size: Side of the square
+        Atributes:
+                size: Side of the square
         """
         self.__size = size
     
