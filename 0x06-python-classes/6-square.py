1682a9a92eb037f2d0ee798234eb57bfa2e8b0a5
martincorredor <dev.martincorredor@gmail.com>
2021-01-05 23:21:59 -0500

    Prints the square without some places

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Module 6-square
+Define class Square
+"""
+
+
+class Square:
+    """Class for size square """
+    def __init__(self, size=0, position=(0, 0)):
+        """ Initializes square"""
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """Calculating the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or len(value) != 2 or \
+           type(value[0]) != int or type(value[1]) != int or \
+           value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
