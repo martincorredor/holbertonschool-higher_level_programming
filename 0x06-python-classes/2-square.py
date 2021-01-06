afe51f938281895cb63d84aab51d50003bc81254
martincorredor <dev.martincorredor@gmail.com>
2021-01-05 19:43:03 -0500

    Verifies the size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module 2-square
+Define class Square
+"""
+
+
+class Square:
+    """
+    Define a square
+    Args:
+        size : Side of a square
+    """
+    def __init__(self, size = 0):
+        """
+        Initializes square
+        Atributes:
+                 size : Side of a square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+        
