591da3df551b00e5cd75fc0e83f4df5642fa1c87
martincorredor <dev.martincorredor@gmail.com>
2021-01-19 18:13:14 -0500

    Check inherent of an object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module 2
+Module 2-is_same_class
 """
 
 
@@ -10,7 +10,7 @@ def is_same_class(obj, a_class):
         use type() to get specific class
         use isinstance() to get class and any parent classes too
         use issubclass() to get what object is a subclass of
-    
-    This function return True if obj is exactly an instance of specified class
+    Return:
+        True if obj is exactly an instance of specified class
     """
     return type(obj) == a_class
