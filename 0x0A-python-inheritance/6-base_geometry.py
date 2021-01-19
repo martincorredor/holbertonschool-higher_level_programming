77ea79bdf95de09675bceebcad0fb34ab6ad09ce
martincorredor <dev.martincorredor@gmail.com>
2021-01-19 17:36:07 -0500

    Raise an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 6
+Contain a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """function that raise an exception"""
+        raise Exception("area() is not implemented")
