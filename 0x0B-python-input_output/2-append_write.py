f27cc4d628ea1303c1f8ce77cdf1f8fee958a38d
martincorredor <dev.martincorredor@gmail.com>
2021-01-20 17:37:17 -0500

    Append a text at the end of a text file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-Module 2-append_write.py
-"""
+"""Module for append_write function."""
 
 
 def append_write(filename="", text=""):
-    """ Appends a string at the end of a text file and
-    Returns the number of characters added
-    """
-    with open(filename, mode="a") as f:
-    return (f.write(text))
+    """This function appends a strings at the end of a text file."""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
