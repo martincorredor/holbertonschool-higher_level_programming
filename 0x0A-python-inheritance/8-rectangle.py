733f2f31aabcb3d41c5aeeffb88bfd786d6afb77
martincorredor <dev.martincorredor@gmail.com>
2021-01-19 19:11:16 -0500

    Validates and initializes width and height

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,9 +6,12 @@ Module 8-rectangle.py
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Class that initializes with and height of rectangle"""
     def __init__(self, width, height):
+        """validate and initialize width and height"""
+
         super().integer_validator("width", width)
         self.__width = width
         super().integer_validator("height", height)
