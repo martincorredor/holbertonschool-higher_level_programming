902df49d19c8a57df3ced6280a45b6e2d258083a
martincorredor <dev.martincorredor@gmail.com>
2021-01-20 16:06:10 -0500

    Writes a string to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module 1-write_file.py"""
+
+
+def write_file(filename="", text=""):
+    """This function writes a string to a text file."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return (f.writes(texti))
