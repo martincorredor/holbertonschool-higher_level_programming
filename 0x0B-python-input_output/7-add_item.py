4e6706af2bc759db468fabb794458debffd73c92
martincorredor <dev.martincorredor@gmail.com>
2021-01-20 18:26:20 -0500

    Adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Module 9-add_item"""
-
-
-from sys import argv
+"""Module for appends elements in a json file."""
+import json
+import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-
 try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_content = []
+    container = load_from_json_file('add_item.json')
+except:
+    container = []
 
-save_to_json_file(existing_content + argv[1:], filename)
+for i in range(1, len(sys.argv)):
+    container.append(sys.argv[i])
+save_to_json_file(container, 'add_item.json')
