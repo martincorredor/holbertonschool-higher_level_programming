3a9edddea5025901205b95f2152eb9328ebea708
martincorredor <dev.martincorredor@gmail.com>
2021-01-19 19:42:58 -0500

    Inherits from class inheritezed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """
-Module 10-square.py
+Module 10-square
 """
 
 
-Rectangle: __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class inherits from Rectangle, who inherits from BaseGeometry"""
+    """inherits from Rectangle, who inherits from BaseGeometry
+    methods:
+        __init__(self, size)
+    """
     def __init__(self, size):
-        """validates and initializes the size value"""
+        """initializes size
+        Args:
+            size (int): private
+        """
         self.integer_validator("size", size)
-        super.__init__(size, size)
+        super().__init__(size, size)
         self.__size = size
