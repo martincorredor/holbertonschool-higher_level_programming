e6840da2c92d1d261b3c90055d213ff81adaa544
martincorredor <dev.martincorredor@gmail.com>
2021-01-26 14:48:31 -0500

    Add square class initialized

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,10 +9,22 @@ class Square(Rectangle):
     """Class that defines an square"""
     def __init__(self, size, x=0, y=0, id=None):
         """Constructor"""
-        super().__init__(id, x, y, width, height)
+        super().__init__(size, size, x, y, id)
         self.size = size
 
+    @property
+    def size(self):
+        """Getter size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter size - sets width and height as size"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
+        """Prints [Square] (<id>) <x>/<y> - <size>"""
         return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
-                self.__class__.__name__, self.id,
-                self.__x, self.__y, self.size)
+            self.__class__.__name__, self.id, self.x, self.y,
+            self.size)
