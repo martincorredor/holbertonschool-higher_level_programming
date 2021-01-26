87a41ae24694f0416bd87f9176f57bed0c9d5bf9
martincorredor <dev.martincorredor@gmail.com>
2021-01-26 16:31:13 -0500

    Loads the JSON string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ Module base.py
 """
 
 
+import json
+
+
 class Base:
     """Base class"""
     __nb_objects = 0
@@ -13,3 +16,35 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return a JSON string representation"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        This method writes the JSON string representation
+        of a list of instances(objects) to a file.
+        """
+        json_file = cls.__name__ + ".json"
+        empty_list = []
+
+        if list_objs is not None:
+            for run in list_objs:
+                empty_list.append(run.to_dictionary())
+
+        with open(json_file, "w") as f:
+            f.write(cls.to_json_string(empty_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of the JSON string representation"""
+        if json_string is None or json_string == 0:
+            json_string = "[]"
+        return json.loads(json_string)
+
