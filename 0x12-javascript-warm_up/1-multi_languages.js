6055f7aebb8381727e9c14ae54afde0f7c68e3b9
martincorredor <dev.martincorredor@gmail.com>
2021-03-22 16:51:55 -0500

    Prints some strings

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Write a script that prints 3 lines
-const varC = "C is fun";
-const varP = "Python is cool";
-const myVar = "JavaScript is amazing";
+const varC = 'C is fun';
+const varP = 'Python is cool';
+const myVar = 'JavaScript is amazing';
 console.log(varC);
 console.log(varP);
 console.log(myVar);
