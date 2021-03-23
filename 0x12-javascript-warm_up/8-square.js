860214f878346973cc802abb2f4f2d8d17d057d1
martincorredor <dev.martincorredor@gmail.com>
2021-03-22 20:36:53 -0500

    Print a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// Print a square with values of arguments passet to it
+const size = parseInt(process.argv[2]);
+if (isNaN(size)) {
+    console.log('Missing size');
+} else {
+    for (let i=0; i < size; i++) {
+        console.log('X'.repeat(size));
+    }
+}
