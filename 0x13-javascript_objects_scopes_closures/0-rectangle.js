5cbcb652a198532a846c8e38cd1b65d40b8e22b2
martincorredor <dev.martincorredor@gmail.com>
2021-03-23 16:28:44 -0500

    Create a empty class

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,1 +1,4 @@
 #!/usr/bin/node
+// Write an empty class Rectangle
+// module.exports es un objeto que el módulo actual devuelve cuando es "requerido" en otro programa o módulo.
+module.exports = class Rectangle { };
