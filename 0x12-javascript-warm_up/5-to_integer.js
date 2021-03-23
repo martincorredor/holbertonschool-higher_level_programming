1e05254c60e041015df2fe3de406f125cee6756a
martincorredor <dev.martincorredor@gmail.com>
2021-03-22 20:00:47 -0500

    converts the argument passed to it into a number and print it

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-//converts the argument passed to it into a number and print it
-
+// converts the argument passed to it into a number and print it
 const num = parseInt(process.argv[2]);
 if (isNaN(num)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
   console.log(`My number: ${num}`);
 }
