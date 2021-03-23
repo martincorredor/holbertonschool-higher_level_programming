b0e5e64fa3b6fc8849722d6dfc424b64cb7513fd
martincorredor <dev.martincorredor@gmail.com>
2021-03-22 20:03:14 -0500

    loop an array and print the items

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-//
-
-const MyVar = ["C is fun", "Python is cool", "JavaScript is amazing"];
+// loop an array and print the items
+const MyVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (i in MyVar) {
   console.log(MyVar[i]);
 }
