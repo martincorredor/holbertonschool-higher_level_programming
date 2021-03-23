1bb49b8b7257557d2fc92720882c307446c76d90
martincorredor <dev.martincorredor@gmail.com>
2021-03-22 22:33:04 -0500

    Search the second biggest number in list

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// Searches the second biggest number in the list of arguments
+const number = process.argv;
+number.splice(0, 2);
+if (number.length < 2) {
+  console.log('0');
+} else {
+  number.sort((a, b) => a - b);
+  console.log(number[number.length - 2]);
+}
