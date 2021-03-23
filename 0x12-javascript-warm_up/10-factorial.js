8faef409f377b3a2507e006d42168c3db9424091
martincorredor <dev.martincorredor@gmail.com>
2021-03-22 21:51:23 -0500

    Calculate and prints the factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// Computes and prints a factorial
+const number = parseInt(process.argv[2]);
+
+if (Number.isNaN(number) || number === 0 || number === 1) {
+  console.log(parseInt('1'));
+} else {
+  console.log(factorial(number));
+}
+function factorial (n) {
+  if (n > 1) {
+    return n * factorial(n - 1);
+  }
+  return 1;
+}
