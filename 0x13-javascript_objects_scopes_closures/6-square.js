ab507a598ea3bb171e1ccc81557002553cf91aef
martincorredor <dev.martincorredor@gmail.com>
2021-03-23 23:24:45 -0500

    Print the square with user parameter

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+// Write a class Square that defines an square and inherits from Rectangle class
+const Rectangle = require("./4-rectangle");
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  // Method that prints the rectangle with parameter c or 'X'
+  charPrint (c) {
+    if (c === undefined) {
+      c = "X";
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
