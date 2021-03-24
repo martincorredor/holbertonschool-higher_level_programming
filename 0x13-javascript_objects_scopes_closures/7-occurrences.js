75feaa7afea28bdb15372120e6ebe3becf621905
martincorredor <dev.martincorredor@gmail.com>
2021-03-23 23:37:23 -0500

    Searches ocurrences of element in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// Write a function that returns the number of ocurrences in a list
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i in list) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
