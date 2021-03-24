f6571fcb02fc64ade54cea9d32d8fa5aea2cf5c8
martincorredor <dev.martincorredor@gmail.com>
2021-03-23 23:45:35 -0500

    Reverses a list without reverse function

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+// Write a function that reverses a list
+exports.esrever = function (list) {
+    let copyList = [];
+    for (let i = list.length - 1; i >= 0; i --) {
+        copyList.join(list[i]);
+    }
+    return copyList;
+}
