0443c1fda0c57b6aa465807b17160db2151f122e
martincorredor <dev.martincorredor@gmail.com>
2021-03-23 23:52:25 -0500

    Printed arguments and number of times that function is used

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// Prints the number of arguments already printed and the new argument value
+module.exports.logMe = function (item) {
+  console.log(`${module.exports.logMe.count}: ${item}`);
+  module.exports.logMe.count++;
+};
+
+module.exports.logMe.count = 0;
