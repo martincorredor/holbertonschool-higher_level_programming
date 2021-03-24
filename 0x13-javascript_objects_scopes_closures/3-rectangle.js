d2fa3259aa3b4f5dd5a7a9cc377cb7ea4fd7a18b
martincorredor <dev.martincorredor@gmail.com>
2021-03-23 22:52:02 -0500

    Prints the rectangle with 'X'

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+//
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && w !== undefined && h !== undefined) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+// Method that prints the rectangle with 'X' 
+  print () {
+    for (let i=0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+    }
+  }
+};
