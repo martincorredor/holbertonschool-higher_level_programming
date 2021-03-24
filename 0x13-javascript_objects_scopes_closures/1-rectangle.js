828f89738914ea21caa7e9f0149645ba69d82e75
martincorredor <dev.martincorredor@gmail.com>
2021-03-23 22:38:35 -0500

    Write a class with constructor function

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Write a class with constructor function that defines two propeties
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
