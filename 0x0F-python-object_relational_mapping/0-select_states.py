4b048f636fe229df3b54546700812a11db9f6b9b
martincorredor <dev.martincorredor@gmail.com>
2021-03-27 22:10:19 -0500

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 ''' This module lists all states from the database hbtn_0e_0_usa '''
+
 import sys
 import MySQLdb
 
-
 if __name__ = "__main__":
-
     # Estableciendo la conexion
     # Los argumentos se reciben de la linea de comandos
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
@@ -15,10 +14,9 @@ if __name__ = "__main__":
     # Este método permite usar otras funciones integradas
     cur = db.cursor()
     # Ejecutando una funcion de MySQL con la función execute()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     # Obteniendo las filas de la consulta y se retorna una lista con fetchall()
     rows = cur.fetchall()
-    # Recorriendo la lista para imprimir el contenido de la consulta
     for i in rows:
         print(i)
     # Cerrando la conexion
