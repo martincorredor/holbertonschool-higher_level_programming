620e2c6bf763226aa5c9f5c006721131925579a0
martincorredor <dev.martincorredor@gmail.com>
2021-03-27 22:01:13 -0500

    lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+This module lists all states from the database hbtn_0e_0_usa
+'''
+
+
+if __name__ = "__main__":
+    import sys
+    import MySQLdb
+    # Estableciendo la conexion
+    # Los argumentos se reciben de la linea de comandos
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    # Creando un objeto "cursor" con la función cursor()
+    # Este método permite usar otras funciones integradas
+    cur = db.cursor()
+    # Ejecutando una funcion de MySQL con la función execute()
+    cur.execute("SELECT * FROM states")
+    # Obteniendo las filas de la consulta y se retorna una lista con fetchall()
+    rows = cur.fetchall()
+    # Recorriendo la lista para imprimir el contenido de la consulta
+    for i in rows:
+        print(i)
+    # Cerrando la conexion
+    cur.close()
+    db.close()
