08b7b3bbd9edaea9a86b7dcea61ee280409c51f8
martincorredor <dev.martincorredor@gmail.com>
2021-03-27 22:18:10 -0500

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,21 +4,13 @@
 import MySQLdb
 from sys import argv
 
-if __name__ = "__main__":
-    # Estableciendo la conexion
-    # Los argumentos se reciben de la linea de comandos
-    db = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                         user=argv[1], passwd=argv[2], db=argv[3])
-
-    # Creando un objeto "cursor" con la función cursor()
-    # Este método permite usar otras funciones integradas
-    cur = db.cursor()
-    # Ejecutando una funcion de MySQL con la función execute()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    # Obteniendo las filas de la consulta y se retorna una lista con fetchall()
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    # Cerrando la conexion
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
-    db.close()
+    conn.close()
