a8f017b4f46db51a0833e67496e6ac98e86db03d
martincorredor <dev.martincorredor@gmail.com>
2021-03-28 22:01:09 -0500

    Add advanced task

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines City ORM object"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    """City ORM class"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
