31eeca9ba291481dcfcfc9df5137a45767b45ed3
martincorredor <dev.martincorredor@gmail.com>
2021-04-09 00:20:07 -0500

    |ADDED| Advanced task

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,1 +1,3 @@
-#!/bin/bash
+#!/bin/bash 
+# Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s "$1" --data @"$2" -H "Content-Type:application/json"
