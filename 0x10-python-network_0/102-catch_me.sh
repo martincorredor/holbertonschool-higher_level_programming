63538d695e0d58c1522aec7df502316a81dbd102
martincorredor <dev.martincorredor@gmail.com>
2021-04-09 00:23:08 -0500

    |ADDED| Advanced task

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# Bash script that makes a request to 0.0.0.0:5000/catch_me and respond You got me!
+curl -s 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -L -H "Origin: HolbertonSchool"
