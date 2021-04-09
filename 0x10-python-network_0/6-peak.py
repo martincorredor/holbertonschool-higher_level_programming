b5ccc3434b2a07fe92d87bde7ead060f3617deab
martincorredor <dev.martincorredor@gmail.com>
2021-04-08 23:26:23 -0500

    |ADDED| Create a function that return de max value in a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Module that contain find_peak function"""
+
+
+def find_peak(list_of_integers):
+    """Sort a list, find and return the maxime value in the list"""
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    else:
+        return None
