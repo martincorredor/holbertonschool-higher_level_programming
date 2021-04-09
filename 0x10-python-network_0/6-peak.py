64e0f0a1c2ee7f077b2140d78fdf5057b73a9bc8
martincorredor <dev.martincorredor@gmail.com>
2021-04-08 23:31:40 -0500

    |FIXED| Fix check for list existence

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,7 @@
 
 def find_peak(list_of_integers):
     """Sort a list, find and return the maxime value in the list"""
-    if list_of_integers:
+    if bool(list_of_integers):
         list_of_integers.sort()
         return list_of_integers[-1]
     else:
