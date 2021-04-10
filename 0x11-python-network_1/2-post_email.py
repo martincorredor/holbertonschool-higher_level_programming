554bdd891e78b15915c7bcb305436b00a80c887e
martincorredor <dev.martincorredor@gmail.com>
2021-04-09 20:05:54 -0500

    |ADDED| Takes an URL and email, sends a POST and chows the body

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter
+and displays the body of the response
+"""
+
+import urllib.request
+import urllib.parse
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
