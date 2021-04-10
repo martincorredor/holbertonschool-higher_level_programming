71ba65b851d4bb39025fedb3c00553aa0239ff8e
martincorredor <dev.martincorredor@gmail.com>
2021-04-09 23:07:27 -0500

    |ADDED| Takes your github credentials and shows your ID

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
