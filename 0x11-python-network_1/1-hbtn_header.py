dbef4d203a1d2ba8ee71101395bae3467037aba8
martincorredor <dev.martincorredor@gmail.com>
2021-04-09 19:58:11 -0500

    |FIXED| Sends a request to the URL and show the variable found in the header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,6 +8,6 @@ from sys import argv
 import urllib.request
 
 if __name__ == "__main__":
-    req = urllib.request.Request(argv[1])
-    with urllib.request.urlopen(req) as response:
-        print(response.getheader('X-Request-Id'))
+    with request.urlopen(argv[1]) as response:
+        result = response.info()
+        print(result.get("X-Request-Id"))
