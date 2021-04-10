1a8380b49ddd4ec5e501506b28a210ac93e7e5e0
martincorredor <dev.martincorredor@gmail.com>
2021-04-09 20:02:37 -0500

    |FIXED| Executable file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-This module takes in a URL, sends a request to the URL and 
-displays the value of the X-Request-Id variable found in the header of the response
+request url with argv
+using the module urllib
 """
 
+from urllib import request
 from sys import argv
-import urllib.request
 
 if __name__ == "__main__":
     with request.urlopen(argv[1]) as response:
